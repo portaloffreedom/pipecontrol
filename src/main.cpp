773bb62b6fc92fad744c5dcd5c68d2a9c9db45f4
Matteo De Carlo <matteo.dek@gmail.com>
2021-10-01 14:45:32 +0200

    Update to version 0.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
   QGuiApplication app(argc, argv);
   app.setApplicationName("PipeControl");
-  app.setApplicationVersion(PROJECT_VERSION);
+  app.setApplicationVersion(PIPECONTROL_VERSION);
   app.setQuitOnLastWindowClosed(true);
   app.setWindowIcon(QIcon(QStringLiteral(INSTALL_PREFIX"/share/icons/pipecontrol.png")));
 
