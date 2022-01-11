992ddb920827fa5a703a5aa924a200ff99ee2351
Matteo De Carlo <matteo.dek@gmail.com>
2022-01-11 18:50:58 +0100

    Cleanup and license

--- src/pw/qpipewirelink.h
+++ src/pw/qpipewirelink.h
@@ -1,5 +1,20 @@
-#ifndef QPIPEWIRELINK_H
-#define QPIPEWIRELINK_H
+/*
+ * This file is part of the pipecontrol project.
+ * Copyright (c) 2022 Matteo De Carlo.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 
 #include <qobject.h>
 #include <spa/utils/dict.h>
@@ -55,5 +70,3 @@ public:
 };
 
 std::ostream& operator<< (std::ostream& out, const QPipewireLink& link);
-
-#endif // QPIPEWIRELINK_H
--- src/pw/qpipewireport.h
+++ src/pw/qpipewireport.h
@@ -1,5 +1,20 @@
-#ifndef QPIPEWIREPORT_H
-#define QPIPEWIREPORT_H
+/*
+ * This file is part of the pipecontrol project.
+ * Copyright (c) 2022 Matteo De Carlo.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 
 #include <qobject.h>
 
@@ -41,5 +56,3 @@ Q_SIGNALS:
 private:
     uint32_t m_m_id;
 };
-
-#endif // QPIPEWIREPORT_H
